57160643d0d765277d52b49760c7f27a48f530c8
Yanshika <yanshikapahwa86@gmail.com>
2025-03-14 19:44:00 +0530

    changes in svg url

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -18,7 +18,7 @@ const MovieCard = ({
         <h3>{title}</h3>
         <div className="content">
           <div className="rating">
-            <img src="/star.svg" alt="star-icon" />
+            <img src="/movie-search/star.svg" alt="star-icon" />
             <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
           </div>
           <span>•</span>
